7883a462cc357400da19743330a57acaa187bdbc
Magali Fabri <magali_90@live.nl>
2022-04-30 07:09:21 +0200

    Add animations to intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,23 +1,70 @@
 import React from 'react';
+import {motion} from "framer-motion";
+import AnimatedSvgName from "./AnimatedSvgName";
 import '../styles/Intro.scss';
 
 
 const Intro = () => {
+
+    const childVariant = {
+        startState: {
+            opacity: 0,
+            y: 5,
+        },
+
+        endState: {
+            opacity: 1,
+            y: 0,
+        },
+    };
+
+   const transitionVariant = {
+       duration: .75,
+       ease: "easeOut",
+       delay: 2.75,
+   };
+
+   const staggerAmount = .25;
+
+
     return (
-        <section className="intro">
-            <div className="heading">
-                <p className="intro__title heading__header">
-                    Magali Fabri
-                </p>
-                <p className="intro__subtitle heading__subheader">
+        <motion.section className="intro"
+                        initial="startState" // for children
+                        animate="endState" // for children
+        >
+            <motion.div className="heading">
+                <AnimatedSvgName/>
+
+                {/*<p className="intro__title heading__header">*/}
+                {/*    Magali Fabri*/}
+                {/*</p>*/}
+
+                <motion.p className="intro__subtitle heading__subheader"
+                          variants={childVariant}
+                          transition={transitionVariant.delay += staggerAmount, {...transitionVariant}}
+                >
                     full stack web dev student
-                </p>
-            </div>
-            <p className="intro__description">
-                Little intro. Nam quis neque at nunc vestibulum mollis. Morbi sit amet ipsum ac justo dignissim elementum. Proin mollis mauris vitae arcu feugiat, a ornare quam iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse bibendum risus mattis leo faucibus, id luctus ligula varius.
-            </p>
-            <a className="intro__contact btn" href="todo">contact</a>
-        </section>
+                </motion.p>
+            </motion.div>
+
+            <motion.p className="intro__description"
+                      variants={childVariant}
+                      transition={transitionVariant.delay += staggerAmount, {...transitionVariant}}
+            >
+                Little intro: what can people expect to find on this page? Morbi
+                sit amet ipsum ac justo dignissim elementum. Proin mollis mauris
+                vitae arcu feugiat, a ornare quam iaculis. Interdum et malesuada
+                fames ac ante ipsum primis in faucibus. Suspendisse bibendum
+                risus mattis leo faucibus, id luctus ligula varius.
+            </motion.p>
+
+            <motion.a className="intro__contact btn" href="todo"
+                      variants={childVariant}
+                      transition={transitionVariant.delay += staggerAmount, {...transitionVariant}}
+            >
+                contact
+            </motion.a>
+        </motion.section>
     );
 };
 
