e1e1e8729e20126927c8b23787baae1e3cb42ef8
Magali Fabri <magali_90@live.nl>
2022-05-03 11:47:28 +0200

    Add basic layout styling to navbar and hide/show it on scroll down/up

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useViewportScroll} from "framer-motion";
 import '../styles/Nav.scss';
 
 
 const Nav = () => {
+    const {scrollYProgress} = useViewportScroll();
+    const [scrollingDown, setScrollingDown] = useState(false);
+
+
+    useEffect(() => scrollYProgress.onChange(v => {
+        setScrollingDown(scrollYProgress.getVelocity() > 0)
+    }), [scrollYProgress]);
+
+
     return (
-        <nav className="nav">
+        <nav className={`nav ${scrollingDown ? 'nav--hidden' : ''}`}>
             <a href="#intro">intro</a>
             <a href="#projects">projects</a>
             <a href="#skills">skills</a>
