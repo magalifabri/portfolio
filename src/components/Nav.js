c51a468da4105d6f4e26d70f580acdf716a748f4
Magali Fabri <magali_90@live.nl>
2022-05-03 14:03:15 +0200

    Make navbar indicate which section is in view

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,19 +6,42 @@ import '../styles/Nav.scss';
 const Nav = () => {
     const {scrollYProgress} = useViewportScroll();
     const [scrollingDown, setScrollingDown] = useState(false);
+    const [inView, setInView] = useState('intro');
 
 
-    useEffect(() => scrollYProgress.onChange(v => {
-        setScrollingDown(scrollYProgress.getVelocity() > 0)
+    const getSectionInView = () => {
+        const projectsSection = document.querySelector('#projects');
+        const skillsSection = document.querySelector('#skills');
+        const contactSection = document.querySelector('#contact');
+
+        if (contactSection.getBoundingClientRect().top < (window.innerHeight / 2)) {
+            return 'contact';
+        } else if (skillsSection.getBoundingClientRect().top < (window.innerHeight / 2)) {
+            return 'skills';
+        } else if (projectsSection.getBoundingClientRect().top < (window.innerHeight / 2)) {
+            return 'projects';
+        } else {
+            return 'intro';
+        }
+    }
+
+    useEffect(() => scrollYProgress.onChange(() => {
+        setScrollingDown(scrollYProgress.getVelocity() > 0);
+
+        setInView(getSectionInView());
     }), [scrollYProgress]);
 
 
     return (
         <nav className={`nav ${scrollingDown ? 'nav--hidden' : ''}`}>
-            <a href="#intro">intro</a>
-            <a href="#projects">projects</a>
-            <a href="#skills">skills</a>
-            <a href="#contact">contact</a>
+            <a className={inView === 'intro' ? 'active' : ''}
+               href="#intro">intro</a>
+            <a className={inView === 'projects' ? 'active' : ''}
+                href="#projects">projects</a>
+            <a className={inView === 'skills' ? 'active' : ''}
+               href="#skills">skills</a>
+            <a className={inView === 'contact' ? 'active' : ''}
+               href="#contact">contact</a>
         </nav>
     );
 };
